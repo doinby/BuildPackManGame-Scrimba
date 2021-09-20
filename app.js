582f9c5853efe77690e629d491e7b9de2ec95037
Chinh <do.inby@gmail.com>
2021-09-21 07:35:54 +1000

    added spawning ghosts

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const layout = [
     1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    5,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,5,
     1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
@@ -34,7 +34,8 @@ const layout = [
 ]
 let pacmanCurrentIndex = 500;
 let isGameOver = false;
-let direction = 1;
+let pacmanDirection = -1;
+let score = 0;
 
 function createBoard() {
     let squareArray = [];
@@ -56,46 +57,98 @@ function createBoard() {
             case 3:
             className = 'power-pellet';
             break;
-            default: className = 'empty';
+            case 4:
+            className = 'empty';
+            break;
+            default:
+            className = 'portal';
         }
         
-        square = `<div class='${className}'><span class="debug-index">${[i]}<span></div>`;
+        square = `<div class='${className}'><span class='debug-index'>${[i]}<span></div>`;
         squareArray = [...squareArray, square];
     }
     $grid.innerHTML = squareArray.join('');
     
     // Show debug index
     // document.querySelectorAll('.debug-index').forEach(el => el.style.display = 'block');
-    // console.log('squareArray:', squareArray.length)
+}
+createBoard();
+const $squares = document.querySelectorAll('.grid div');
+
+// Create pacman
+let $pacman = $squares[pacmanCurrentIndex];
+$pacman.classList.add('pacman');
+
+function createGhosts() {
+    class Ghost {
+        constructor(className, startIndex, speed) {
+            this.className = className;
+            this.startIndex = startIndex;
+            this.speed = speed;
+            this.currentIndex = startIndex;
+            this.isScared = false;
+            this.timerId = NaN;
+        }
+    }
+    
+    const ghosts = [
+        new Ghost("blinky", 348, 250),
+        new Ghost("pinky", 376, 400),
+        new Ghost("inky", 351, 300),
+        new Ghost("clyde", 379, 500),
+    ];
+    
+    ghosts.forEach((ghost) => {
+        const {className, startIndex} = ghost;
+        $squares[startIndex].classList.add(`${className}`, "ghost");
+    });
+}
+createGhosts();
+
+function moveThroughPortal(index) {
+    if(index === 364) {
+        return 391;
+    } else return 364;
 }
 
-createBoard();
+function eatPacDot(index) {
+    $squares[index].classList.remove('pac-dot');
+    $scoreDisplay.textContent = score++;
+}
 
-const $squares = document.querySelectorAll('.grid div');
-let $pacman = $squares[pacmanCurrentIndex];
-$pacman.classList.add("pacman");
+function isWithinBoundaries(position, direction) {
+    if(!(position + width >= width * width && direction === width) || // if object doesnt hit bottom
+    !(position % width === width - 1 && direction === 1) || // if object doesnt hit right wall
+    !(position % width === 0 && direction === -1) || // if object doesnt hit left wall
+    !(position - width < 0 && direction === -width)) {// if object doesnt hit top
+        return true;
+    } else return false;
+}
 
-function animatePacman(direction) {
+function animatePacman(pacmanDirection = -1) {
     // check if pacman is within boundary
-    if(!(pacmanCurrentIndex + width >= width * width && direction === width) || // if he doesnt hit bottom
-    !(pacmanCurrentIndex % width === width - 1 && direction === 1) || // if he doesnt hit right wall
-    !(pacmanCurrentIndex % width === 0 && direction === -1) || // if he doesnt hit left wall
-    !(pacmanCurrentIndex - width < 0 && direction === -width)) {// if he doesnt hit top
+    if(isWithinBoundaries(pacmanCurrentIndex, pacmanDirection)) {
+        // calculate pacman moves
+        $squares[pacmanCurrentIndex].classList.remove('pacman');
+        pacmanCurrentIndex += pacmanDirection;
         
-        // animate moves
-        $squares[pacmanCurrentIndex].classList.remove("pacman");
-        pacmanCurrentIndex += direction;
-        // if he hits wall, back out
-        if($squares[pacmanCurrentIndex].classList.contains('wall')) { 
-            pacmanCurrentIndex -= direction;
-            $squares[pacmanCurrentIndex].classList.add("pacman");
-        } 
-        // else if() {
-        
-        // }
-        else {
-            $squares[pacmanCurrentIndex].classList.add("pacman");
+        // if he hits wall or ghost-lair, back out
+        if ($squares[pacmanCurrentIndex].classList.contains('wall') ||
+        $squares[pacmanCurrentIndex].classList.contains('ghost-lair')) {
+            pacmanCurrentIndex -= pacmanDirection;
         }
+        // if he goes through portal
+        else if($squares[pacmanCurrentIndex].classList.contains('portal')) {
+            pacmanCurrentIndex = moveThroughPortal(pacmanCurrentIndex);
+        }
+        // if he eats pac-dot
+        else if($squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+            eatPacDot(pacmanCurrentIndex);
+        }
+        
+        // draw new pacman
+        $squares[pacmanCurrentIndex].classList.add('pacman');
+        console.log('pacmanCurrentIndex:', pacmanCurrentIndex);
     }
 }
 
@@ -103,20 +156,24 @@ function animatePacman(direction) {
 document.addEventListener('keydown', event => {
     if(!isGameOver) {
         switch(event.code) {
-            case "ArrowDown":
-            direction = width;
+            case 'ArrowDown':
+            pacmanDirection = width;
             break;
-            case "ArrowUp":
-            direction = -width;
+            case 'ArrowUp':
+            pacmanDirection = -width;
             break;
-            case "ArrowLeft": 
-            direction = -1;
+            case 'ArrowLeft': 
+            pacmanDirection = -1;
             break;
-            case "ArrowRight": 
-            direction = 1;
+            case 'ArrowRight': 
+            pacmanDirection = 1;
             break;
             default: return;
         }
-        animatePacman(direction);
+        animatePacman(pacmanDirection);
     }
 });
+
+function animateGhosts() {
+    
+}
